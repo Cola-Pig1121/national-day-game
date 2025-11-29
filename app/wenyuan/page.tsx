6f5b03d536ecfa2dce75798aa3c61b7644896d9b
Cola Pig <123631723+Cola-Pig1121@users.noreply.github.com>
2025-11-29 22:31:52 +0800

    更新 page.tsx

--- app/wenyuan/page.tsx
+++ app/wenyuan/page.tsx
@@ -2,8 +2,8 @@
 export default function Wenyuan() {
   return (
  <h1>文渊的屁股不是新的</h1>
- <button style={{ marginTop: '1rem', padding: '0.5rem 1rem' }}>
-        认同
+ <button>
+认同
       </button>
 )
 }
